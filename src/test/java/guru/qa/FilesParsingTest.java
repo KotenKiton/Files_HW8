cedb554a7cc43fe6b7e31f4d8d48ec7db89dee0f
Koten <Kotenkiton@gmail.com>
2022-06-15 14:10:08 +0300

    Completed uploaded test

---
+++ src/test/java/guru/qa/FilesParsingTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+public class FilesParsingTest {
+
+    @Test
+    void parserPdfTest() {
+
+    }
+
+}
--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -1,5 +1,6 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideFilesTest {
+
     @Test
     void SelenideDownloadTest() throws Exception {
         // чтобы скачать файл должно быть написать href + ссылка в девтулсе.
@@ -20,6 +22,21 @@ public class SelenideFilesTest {
             assertThat(new String(is.readAllBytes(), StandardCharsets.UTF_8))
                     .contains("This repository is the home of the next generation of JUnit");
         }
+    }
+
+    // Корень для поиска файлов у класс лоадера - папка resources
+    @Test
+    void uploadSelenideTest() {
+        Selenide.open("https://the-internet.herokuapp.com/upload");
+        Selenide.$("input[type='file']") // SUPER BAD PRACTICE
+                // SUPER BAD PRACTICE .uploadFile(new File("C:\\Users\\koten\\IdeaProjects\\Files_HW8\\src\\test\\resources\\Files\\1.txt"));
+                .uploadFromClasspath("Files\\1.txt");
+        Selenide.$("#file-submit").click();
+        Selenide.$("div.example").shouldHave(Condition.text("File Uploaded!"));
+        Selenide.$("#uploaded-files").shouldHave(Condition.text("1.txt"));
+
+
+
     }
 
 
